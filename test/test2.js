03e06607e36055195f4c469ef4ee32c5d1c0c1d0
chessgogogo <chessgogogo@gmail.com>
2023-02-18 22:11:34 +0800

    Create test2.js

---
+++ test/test2.js
@@ -0,0 +1,34 @@
+class Scratch3NewBlocks {
+    constructor (runtime) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            id: 'newblocks',
+            name: 'New Blocks',
+            blocks: [
+                {
+                    opcode: 'writeLog',
+                    blockType: BlockType.COMMAND,
+                    text: 'log [TEXT]',
+                    arguments: {
+                        TEXT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    writeLog (args) {
+        const text = Cast.toString(args.TEXT);
+        log.log(text);
+    }
+}
+
+module.exports = Scratch3NewBlocks;
