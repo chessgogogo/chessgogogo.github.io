945c383827f50a089c62e14efded55c9fb41197c
chessgogogo <chessgogogo@gmail.com>
2023-02-18 23:54:47 +0800

    Update test2.js

--- test/test2.js
+++ test/test2.js
@@ -1,33 +1,61 @@
-class Scratch3NewBlocks {
-    constructor (runtime) {
-        this.runtime = runtime;
+class ScratchFetch {
+    constructor() {
     }
-
-    getInfo () {
+    
+    getInfo() {
         return {
-            id: 'newblocks',
-            name: 'New Blocks',
-            blocks: [
-                {
-                    opcode: 'writeLog',
-                    blockType: BlockType.COMMAND,
-                    text: 'log [TEXT]',
-                    arguments: {
-                        TEXT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: "hello"
-                        }
-                    }
-                }
-            ],
-            menus: {
-            }
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "fetchURL",
+                            "blockType": "reporter",
+                            "text": "fetch data from [url]",
+                            "arguments": {
+                                "url": {
+                                    "type": "string",
+                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
+                ],
         };
     }
-
-    writeLog (args) {
-        const text = Cast.toString(args.TEXT);
-        log.log(text);
+    
+    fetchURL({url}) {
+        return fetch(url).then(response => response.text())
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
     }
 }
-Scratch.extensions.register(new Scratch3NewBlocks());
+
+Scratch.extensions.register(new ScratchFetch())
